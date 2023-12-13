b1d5f9f61d46479b76749226a3d0655bbb75a7e0
rohanjadhav358 <153717132+rohanjadhav358@users.noreply.github.com>
2023-12-13 09:50:29 +0000

    container config

---
+++ js_practical/varible.js
@@ -0,0 +1,7 @@
+const accuntId = 64646
+let accuntEmail = "rohanjadhav464"
+var accuntPassword ="12345678"
+console.log(accuntEmail);
+accuntEmail = "jadhav"
+accuntPassword = "9011878821"
+console.table([accuntEmail,accuntPassword,])
